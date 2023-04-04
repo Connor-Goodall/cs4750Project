f298fd6f04385c301f0492a46886452523abce63
Connor Goodall <burningshock83@gmail.com>
2023-04-04 01:36:10 -0400

    Added most of the update profile page. Some bugs need to be fixed

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
         <?php else : ?>
             <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-                <a class="nav-link active" href="updateProfile.php">Profile</a>
+                <a class="nav-link active" href="userProfile.php">Profile</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link active" href="logout.php">Logout</a>
