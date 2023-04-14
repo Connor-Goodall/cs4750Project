d7da899e4cbef71916c654ebc7b400bae0bc5e87
ebranch13 <72311862+ebranch13@users.noreply.github.com>
2023-04-14 17:39:02 -0400

    upVOTE AND downvote works, author wip

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php 
 require("connect-db.php");
 require("club-db.php");
+error_reporting(E_ERROR | E_PARSE);
 
 function getUserPosts ($ID)  // given a search for a club name, returns relevant info about all the posts created by that club
 {
     global $db;
-    $query = "SELECT `Title`, `Body_Text`, `Name`, `Post_Date`, `Picture`, `Upvotes`, `Downvotes` 
+    $query = "SELECT `Title`, `Body_Text`, `Name`, `Post_Date`, `Picture`, `Upvotes`, `Downvotes`, `Post_ID`, p.computing_id AS author 
     FROM `Post` AS p NATURAL JOIN `Club` INNER JOIN `MemberOf` AS m ON m.Club_ID = p.Club_ID WHERE m.computing_id = :computingID ORDER BY `Name`,  `Post_Date`";
     $statement = $db->prepare($query);
     $statement->bindValue(':computingID', $ID);
@@ -19,6 +20,23 @@ function getUserPosts ($ID)  // given a search for a club name, returns relevant
 // home page has my bulletin
 // club info page has a button for the clubs individual bulletin page
 
+function upvote($pid){
+    global $db;
+    $query = "UPDATE Post SET `Upvotes` = `Upvotes` + 1 WHERE `Post_ID`= :pid;";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':pid', $pid);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function downvote($pid){
+    global $db;
+    $query = "UPDATE Post SET `Downvotes` = `Downvotes` + 1 WHERE `Post_ID`= :pid;";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':pid', $pid);
+    $statement->execute();
+    $statement->closeCursor();
+}
 
 function printPosts ($array) // prints each post
 {
@@ -32,24 +50,45 @@ function printPosts ($array) // prints each post
             echo '<h3 style="text-align: center" >Posts by ' . $row['Name'] . '<h3>';
             $firstClub = $row['Name'];
            }
-                
-                
+           $pid = $row['Post_ID'];
+
                 echo '<div class="card mx-auto" style="width: 50rem; text-align: center">';
                 echo '<div class="card-body">';
                     echo '<h5 class="card-title" style="font-size:22px">' . $row['Title'] . '</h5>';
-                    echo '<p class="text-muted" style="font-size:12px"> Posted:  '. $row['Post_Date'] . '</p>';
+                    echo '<p class="text-muted" style="font-size:12px"> Posted:  '. $row['Post_Date'] . ' by '. $row['author'] . '</p>';
                     echo '<p class="card-text" style="font-size:18px">' . $row['Body_Text'] . '</p>';
-                    echo '<p class="card-footer" style="font-size:15px"> Upvotes:  ' . $row['Upvotes'] . '             Downvotes:  ' . $row['Downvotes'] . '</p'; 
+                    echo '<div style="text-align:  left">';
+                    echo '<div  class = "btn-group";>';
+                        echo '<form action = "index.php" method = "post"> 
+                            <input type = "hidden" name = "upbtn" value='.$pid . '>
+                            <input type = "submit" class = "btn btn-success mx-2" data-inline="true" name = "actionBtn" value = "Upvotes:  ' . $row['Upvotes'] .'"/>
+                            </form>'; 
+                        echo '<form action = "index.php" method = "post"> 
+                        <input type = "hidden" name = "downbtn" value='.$pid . '>
+                        <input type = "submit" class = "btn btn-danger" data-inline="true" name = "actionBtn" value = "Downvotes:  ' . $row['Downvotes'] . '"/>
+                        </form>';  
+                echo '</div>';
+                echo '</div>'; 
                 echo '</div>';
             echo '</div>';
             echo '</div>';
            
-            
         }
     } else {
         echo '<h3>No posts found</h3>';
     }
 }
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST["upbtn"]) {
+        upvote($_POST["upbtn"]);
+    }
+    if ($_POST["downbtn"]){
+        downvote($_POST["downbtn"]);
+    }
+    
+}
+
 ?>
 
 <!DOCTYPE html>
