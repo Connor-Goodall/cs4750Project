5a28cee0df05b81c3004f8976a2de881bd00e9d9
Connor Goodall <burningshock83@gmail.com>
2023-04-25 23:28:01 -0400

    Changed the font and added background colors

--- register.php
+++ register.php
@@ -47,8 +47,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet'>
 </head>
-<body style = "background: #5be7a9;">
+<body style = "background: #5be7a9; font-family: Lato;">">
     <div>
         <?php include("header.php") ?>
     </div>
